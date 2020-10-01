9812b00d6f4bf06fb313a73de0dc219fa0fa8860
tztokchad <tz@dopex.io>
2020-10-01 19:42:39 +0400

    add arb strat skeletons

---
+++ contracts/arb/strategies/CrvSwrvArb.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.6.10;
+
+import "./FlashLoanStrategy.sol";
+
+contract CrvSwrvArb is FlashLoanStrategy {
+
+    function execute(
+      bytes memory strategyData
+    ) 
+    override 
+    public  {
+    }
+
+    /**
+    * Sweep entire balance on the arb contract back to contract owner
+    */
+    function withdrawBalance(address _token) 
+    public 
+    payable
+    override
+    onlyOwner {
+        // withdraw all ETH
+        msg.sender.call{ value: address(this).balance }("");
+        // withdraw all x ERC20 tokens
+        IERC20(_token).transfer(msg.sender, IERC20(_token).balanceOf(address(this)));
+    }
+
+}
