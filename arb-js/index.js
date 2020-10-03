75af1b311aa5642a3b6742889f376206d2c72754
tztokchad <tz@dopex.io>
2020-10-04 01:42:12 +0400

    init arb-js commit

---
+++ arb-js/index.js
@@ -0,0 +1,28 @@
+require("dotenv");
+
+const Web3 = require("web3");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const Arb = require("./src/arb");
+const Events = require("./src/events");
+
+const {
+  MNEMONIC,
+  PRIVATE_KEY,
+  WEB3_URL,
+  TOKEN_LIST,
+  PROFIT_THRESHOLD,
+  GAS_PRICE
+} = process.env;
+
+(async () => {
+  let web3;
+  if (MNEMONIC) web3 = new Web3(new HDWalletProvider(MNEMONIC, WEB3_URL));
+  else if (PRIVATE_KEY) web3 = new Web3(WEB3_URL);
+  else
+    throw new Error(
+      "no auth set. Please set a mnemonic or private key in .env"
+    );
+  const events = new Events(web3);
+  const arb = new Arb(web3, events);
+})();
